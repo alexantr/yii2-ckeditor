b103631a850913d248d83e188bd904bc1a663f99
Alex Yashkin <alex.yashkin@gmail.com>
2017-05-29 12:06:54 +0300

    Added compatibility with phpunit 6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,11 @@ define('YII_DEBUG', true);
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
+    {
+    }
+}
+
 Yii::setAlias('@tests', __DIR__);
 Yii::setAlias('@alexantr/ckeditor', dirname(__DIR__) . '/src');
